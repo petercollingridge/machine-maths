690b891a57bff732dbe349c645d936b0242a7dcf
Peter Collingridge <peter.collingridge@nanoporetech.com>
2021-08-04 23:07:53 +0100

    Improve conveyor speed

--- src/Items/Items.js
+++ src/Items/Items.js
@@ -2,22 +2,41 @@ import React from 'react';
 import { useSpring, animated } from 'react-spring';
 import Apple from './Apple';
 
-function Items({ count, x, y, gap=30, animating }) {
-    const distance = gap * (count + 1);
-    const timeTaken = 1000 * distance / 25;
+function Items({
+    count,
+    x,
+    y,
+    size = 28,
+    gap=30,
+    animating
+}) {
+    const SPEED = 20 / 1000; // 25 pixels per second
+    const distance = gap * (count + 1) * 2;
+    const timeTaken = distance / SPEED;
 
     const styles = useSpring({
         config: { duration: timeTaken },
         from: { x: 0 },
-        to: { x: gap * (count + 1) },
+        to: { x: distance },
         pause: !animating,
       })
 
     const items = [];
     for (let i = 1; i <= count; i++) {
-        items.push(<Apple key={i} x={x - gap * i} y={y} size={28} />);
+        items.push(<Apple key={i} x={x - gap * i} y={y} size={size} />);
     }
-    return <animated.g style={styles}>{items}</animated.g>;
+    return (
+        <>
+            <defs>
+                <clipPath id="clip-items-left">
+                    <rect x={x - distance} y={y - size - 1} width={distance} height={size + 2} />
+                </clipPath>
+            </defs>
+            <animated.g style={styles} clipPath="url(#clip-items-left)">
+                {items}
+            </animated.g>
+        </>
+    );
 }
 
 export default Items;
